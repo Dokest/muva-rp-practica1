c6ac184b44c993d7f0e2e2459d7fb1116cb48175
Jorge Moreno <jorge.moreno.fernandez@outlook.com>
2023-10-21 13:48:53 +0200

    feat: pca & logistic regression inside everything

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Reconocimiento de patrones: Practica 1
+
+## To test
+- Dimension reduction
+  - [x] PCA
+  - [ ] LDA (H)
+  - [ ] Hyperparameter optimization (J)
+    - [ ] Square...
+  - [ ] LLE (H)
+- Classifying
+  - [ ] KNN (H)
+  - [ ] SVM (J)
+  - [ ] Gradient boosting (H)
+  - [ ] Random forest (J)
+  - [ ] Perceptron* (H)
+  - [ ] K-means (J)
+  - [x] Logistic regression
+  - [ ] Naive Bayes (J)
+
+https://scikit-learn.org/stable/supervised_learning.html#supervised-learning
+
+## Dudas
+- ¿Como usamos los tests? ¿Sacamos 20% del training para despues tener Ground Truth?
+- 
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
+import sklearn
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 def load_labels() -> pd.DataFrame:
@@ -35,4 +41,102 @@ for path in training_paths:
     training_df = training_df.merge(load_training(path), on="id", how="right", suffixes=("", "_" + str(index)))
     index += 1
 
-print(training_df)
+training_labels = training_df[training_df.columns[1]].to_numpy()
+training_np: np = training_df.drop("malignant", axis=1).to_numpy()
+
+# Split training
+kf = KFold(n_splits=5)
+kf.get_n_splits(training_np)
+
+
+def everything(training, training_lbl, testing, testing_lbl, scaler, dimRed, classifier) -> []:
+    # Reduce dimension
+    scaler.fit(training)
+
+    training = scaler.transform(training)
+    testing = scaler.transform(testing)
+
+    dimRed.fit(training)
+
+    training = dimRed.transform(training)
+    testing = dimRed.transform(testing)
+
+    # Classify (predict)
+    classifier.fit(training, training_lbl)
+
+    # Result
+    return classifier.predict(testing)
+
+
+def calculate_scores(real, predicted):
+    f1_score = sklearn.metrics.f1_score(real, predicted)
+    precision = sklearn.metrics.precision_score(real, predicted)
+    recall = sklearn.metrics.recall_score(real, predicted)
+    mse = sklearn.metrics.mean_squared_error(real, predicted)
+    roc = sklearn.metrics.roc_curve(real, predicted)
+
+    print({
+        "f1": f1_score,
+        "precision": precision,
+        "recall": recall,
+        "mse": mse,
+        "roc": roc,
+    })
+
+
+for i, (train_indices, test_indices) in enumerate(kf.split(training_np)):
+    print(f"Fold: {i}")
+
+    training = training_np[train_indices]
+    training_lbl = training_labels[train_indices]
+
+    testing = training_np[test_indices]
+    testing_lbl = training_labels[test_indices]
+
+    scaler = StandardScaler()
+    pca = PCA(n_components=0.85)
+    logisticRegr = LogisticRegression()
+
+    predicted = everything(
+        training=training,
+        training_lbl=training_lbl,
+        testing=testing,
+        testing_lbl=testing_lbl,
+        scaler=scaler,
+        dimRed=pca,
+        classifier=logisticRegr,
+    )
+
+    calculate_scores(testing_lbl, predicted)
+
+    sklearn.metrics.RocCurveDisplay.from_predictions(testing_lbl, predicted)
+    plt.show()
+
+"""
+training = training_np[train_indices]
+training_lbl = training_labels[train_indices]
+
+testing = training_np[test_indices]
+testing_lbl = training_labels[test_indices]
+
+# Reduce dimension
+scaler = StandardScaler()
+scaler.fit(training)
+
+training = scaler.transform(training)
+testing = scaler.transform(testing)
+
+pca = PCA(n_components=0.85)
+pca.fit(training)
+
+training = pca.transform(training)
+testing = pca.transform(testing)
+
+# Classify (predict)
+logisticRegr = LogisticRegression()
+logisticRegr.fit(training, training_lbl)
+
+# Result
+x = logisticRegr.score(testing, testing_lbl)
+print(x)
+"""
