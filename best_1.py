996288644d034e29d56a471d9f319ff1a65dc8db
Hugogp <hugogp@hotmail.es>
2023-11-12 13:59:11 +0100

    fix: sgd and svm parametrize

--- best_1.py
+++ best_1.py
@@ -26,3 +26,19 @@ print(trainer.calculate_scores(predicted))
 {'f1': 0.49223946784922396, 'precision': 0.6235955056179775, 'recall': 0.4065934065934066, 'confusion_matrix': array([[227,  67],
        [162, 111]], dtype=int64), 'mse': 0.4038800705467372, 'roc': (array([0.        , 0.22789116, 1.        ]), array([0.        , 0.40659341, 1.        ]), array([2, 1, 0], dtype=int64))}
    """
+
+"""
+{'accuracy': 0.6049382716049383, 'confusion_matrix': array([[213,  81],
+       [143, 130]], dtype=int64), 'f1': 0.5371900826446281, 'precision': 0.6161137440758294, 'recall': 0.47619047619047616}
+Best Transformer: Pipeline(steps=[('StandardScaler', StandardScaler()),
+                ('PCA', PCA(n_components=0.99))])
+Best Classifier: SGDClassifier(alpha=2, loss='modified_huber', n_jobs=-1, random_state=1)
+"""
+
+"""
+{'accuracy': 0.6225749559082893, 'confusion_matrix': array([[229,  65],
+       [149, 124]], dtype=int64), 'f1': 0.5367965367965368, 'precision': 0.656084656084656, 'recall': 0.4542124542124542}
+Best Transformer: Pipeline(steps=[('StandardScaler', StandardScaler()),
+                ('PCA', PCA(n_components=0.99))])
+Best Classifier: SVC(C=2, degree=2, random_state=1)
+"""
