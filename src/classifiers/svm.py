d070256a0153a93056b4eae24f332fe730eb55e9
Hugogp <hugogp@hotmail.es>
2023-11-12 13:12:53 +0100

    fix: parametrize sgd and svm

--- src/classifiers/svm.py
+++ src/classifiers/svm.py
@@ -1,9 +1,14 @@
-from sklearn import svm
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+from src.utils import stepped_values
 
 
-def classifier_svm():
-    return Pipeline([
-        ("Support Vector Machine", svm.SVC())
-    ])
+def svm_classifier():
+    return {
+        "classifier": [SVC()],
+        "classifier__C": stepped_values(0.5, 2, 0.5),
+        "classifier__kernel": ["linear", "poly", "rbf", "sigmoid", "precomputed"],
+        "classifier__degree": [2, 3, 4],
+        "classifier__gamma": ["scale", "auto"] + stepped_values(1.0, 5.0, 1),
+        "classifier__random_state": [1]
+    }
